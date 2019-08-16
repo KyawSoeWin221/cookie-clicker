cdc9b73ecb2ae0116c558b2b60451c01540d3b4b
KyawSoeWin221 <54158756+KyawSoeWin221@users.noreply.github.com>
2019-08-16 14:15:03 +1000

    Initial commit

---
+++ Assets/Scripts/SellCookies.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellCookies : MonoBehaviour
+{
+    public GameObject textBox;
+    public void ClickTheBotton()
+    {
+        textBox.SetActive(true);
+        GlobleCockie.CookieCount -= 1;
+        GlobleCash.CashCount += 1;
+    }
+}
