b4338f7c08f33dab2fe307c496bf3c3bceaf9ac8
KyawSoeWin221 <54158756+KyawSoeWin221@users.noreply.github.com>
2019-08-30 21:47:04 +1000

    remake

--- Assets/Scripts/MainButtonClick.cs
+++ Assets/Scripts/MainButtonClick.cs
@@ -7,7 +7,7 @@ public class MainButtonClick : MonoBehaviour
     public GameObject textBox;
     public void ClickTheBotton()
     {
-        textBox.SetActive(true);
+        
         GlobleCockie.CookieCount += 1;
     }
 }
--- Assets/Scripts/SellCookies.cs
+++ Assets/Scripts/SellCookies.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class SellCookies : MonoBehaviour
 {
     public GameObject textBox;
+    public Gameobject StatusBox;
+
     public void ClickTheBotton()
     {
-        textBox.SetActive(true);
-        GlobleCockie.CookieCount -= 1;
-        GlobleCash.CashCount += 1;
+        if (GlobleCookie.CookieCount == 0)
+        {
+
+            StatusBox.Getcomponent<text>().text = "Not Enough Cookie to Sell.";
+            StatusBox.GetComponent<animation>().Play("StatusAnimation");
+        }
+        else
+        {
+            GlobleCockie.CookieCount -= 1;
+            GlobleCash.CashCount += 1;
+
+        }
+
+        
+
     }
 }
